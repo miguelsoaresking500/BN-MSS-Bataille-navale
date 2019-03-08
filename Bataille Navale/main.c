f36b6af814183dc05639f9d8010122a9fd5aec54
Miguel Soares <miguel.soares@cpnv.ch>
2019-03-08 09:13:52 +0100

    texte aide

---
+++ Bataille Navale/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    int choix;
+
+
+    printf("__________BATAILLE NAVALE___________\n");
+
+    do{
+       printf("1.Nouvelle Partie");
+       printf("2.Apprendre à joeur");
+       printf("3.Quitter");
+
+       switch {
+
+           case 1 :
+               printf("1.Nouvelle Partie");
+               scanf()
+
+       }
+
+
+
+
+
+    }while (choix<0 || choix>3);
+
+
+
+    return 0;
+}
