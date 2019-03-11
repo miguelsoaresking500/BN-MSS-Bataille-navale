eb5077754dcfe77b1ead6fa1ff7979126113eb84
Miguel Soares <miguel.soares@cpnv.ch>
2019-03-11 08:18:00 +0100

    aide

---
+++ Bataille Navale/Text Aide/cmake-build-debug/CMakeFiles/Repo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Repo.dir/main.c.obj"
+  "Repo.pdb"
+  "Repo.exe"
+  "Repo.exe.manifest"
+  "libRepo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Repo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Bataille Navale/Text Aide/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    //affichage du texte d'aide
+    printf("Texte d'aide\n");
+    return 0;
+}
