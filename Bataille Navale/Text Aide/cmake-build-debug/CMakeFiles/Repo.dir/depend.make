b45c10b974f6d35f64793d8773127c97dc4afc72
Miguel Soares <miguel.soares@cpnv.ch>
2019-03-22 09:35:43 +0100

    update 22.03.2019

--- Bataille Navale/Text Aide/cmake-build-debug/CMakeFiles/Repo.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Repo.
-# This may be replaced when dependencies are built.
--- Bataille navale 2.0/main.c
+++ Bataille navale 2.0/main.c
@@ -5,74 +5,97 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <unistd.h>
+#define X 9
+#define Y 9
+
 
 
 #pragma execution_character_set("UTF-8")
 
-int Grille1[9][9]=
+int Grille1[X][Y] =
         {
-                0,0,0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,1,0,
-                0,0,0,0,0,0,0,0,0,
-                0,0,0,0,3,0,0,0,0,
-                0,0,0,0,3,0,0,0,0,
-                0,0,0,0,3,0,0,0,0,
-                0,0,0,0,0,0,0,0,0,
-                0,0,0,0,0,0,0,0,0,
-                0,0,2,2,0,0,0,0,0
+                1, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
         };
 
-void Game()
-{
+void Game() {
     int x;
     int y;
     printf("  X  A   B   C   D   E   F   G   H   I");
-    for(x= 0;x<9;x++)
-    {
+    for (x = 0; x < X; x++) {
         printf("\n");
 
-        if(x==0)
-        {
+        if (x == 0) {
             printf("   ┌───┬───┬───┬───┬───┬───┬───┬───┬───┐\n  ");
         }
-        if(x!=0)
-        {
+        if (x != 0) {
             printf("   ├───┼───┼───┼───┼───┼───┼───┼───┼───┤\n  ");
         }
 
 
-
-
-        printf("%d",x+1);
-        for(y=0;y<9;y++) {
+        printf("%d", x + 1);
+        for (y = 0; y < X; y++) {
 
             if (Grille1[x][y] == 0) {
 
                 printf("│   ");
+            } else if (Grille1[x][y] == 1) {
+                printf("│   ");
+            } else if (Grille1[x][y] == 2) {
+                printf("│ x ");
+            } else if (Grille1[x][y] == 3) {
+                printf("│  ");
+            } else if (Grille1[x][y] == 4) {
+                printf("│ X ");
             }
-                else if (Grille1[x][y] == 1) {
-                    printf("│  l ");
-                } else if (Grille1[x][y] == 2) {
-                    printf("│ x ");
-                } else if (Grille1[x][y] == 3) {
-                    printf("│  ");
-                } else if (Grille1[x][y] == 4) {
-                    printf("│ X ");
-                }
 
 
         }
-        if(y==9)
-        {
+        if (y == 9) {
             printf("│");
         }
-        if(x==8)
-        {
+        if (x == 8) {
             printf("\n   └───┴───┴───┴───┴───┴───┴───┴───┴───┘");
         }
     }
 }
 
+void shoot() {
+
+    int compteur = 0;
+    int introx;
+    int introy;
+    int test = 0;
+    while (test != 1) {
+        Game();
+        printf("              \nentrez la cordonée horizontale: ");
+        scanf("%d", &introx);
+
+        if (introx == -1) {
+            printf("entrez la cordonée horizontale et faites enter\n");
+            printf("entrez la cordonée horizontale ");
+            scanf("%d", &introx);
+        }
+        printf("entrez la coordonee verticale: ");
+        scanf("%d", &introy);
+
+        if (introy == -1) {
+            printf("entrez la cordonée Verticale et faites enter\n");
+            printf("entrez la cordonée Verticale ");
+            scanf("%d", &introy);
+        }
+        compteur = compteur + 1;
+
+
+    }
+}
 
 
 int main() {
@@ -101,50 +124,46 @@ int main() {
         //Que faire ?
         printf("\nIntroduisez un chiffre de 0 a 2: ");
         scanf("%d", &choix);
-        if(choix <0 || choix > 2)
-        {
+        if (choix < 0 || choix > 2) {
             printf("Ceci n'est pas un choix !");
         }
 
-        switch(choix)
-        {
-            case 0: printf("\nA la prochaine !");
+        switch (choix) {
+            case 0:
+                printf("\nA la prochaine !");
                 return 0;
 
-            case 1: printf("\n\nNouvelle Partie\n");
+            case 1:
+                shoot();
+                printf("\n\nNouvelle Partie\n");
                 printf("\nBievenue sur le mode jeu");
                 printf("\nIntroduire 0 pour revenir en arriere ou 1 pour lancer une partie: \n");
-                scanf("%d",&choix);
-                if (choix == 0)
-                {
-                    choix=+100;
+                scanf("%d", &choix);
+                if (choix == 0) {
+                    choix = +100;
                     break;
-                }
-                else if(choix==1)
-                {
+                } else if (choix == 1) {
                     Game();
-                }
-                else
-                {
+                } else {
                     printf("ce n'est pas un choix !\n");
                     break;
                 }
                 break;
 
 
-            case 2: printf("\n\nInstructions\n");
+            case 2:
+                printf("\n\nInstructions\n");
                 printf("\nLes regles s'affichent...");
                 printf("\nIntroduire 0 pour revenir en arriere: ");
-                scanf("%d",&choix);
-                if (choix == 0)
-                {
-                    choix=+100;
+                scanf("%d", &choix);
+                if (choix == 0) {
+                    choix = +100;
                     break;
                 }
                 break;
         }
 
-    }while (choix < 0 || choix > 3);
+    } while (choix < 0 || choix > 3);
 
     return 0;
 }
